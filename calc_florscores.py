798f5885ea38b2fe3aa55d22e91f252bc7b97b9c
Haoli Yin <haoli@datologyai.com>
2024-07-01 00:21:02 +0000

    Finish FPS impl

--- calc_florscores.py
+++ calc_florscores.py
@@ -19,7 +19,8 @@ image_dir = "data/sampled_datacomp/sampled_datacomp_images"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # model_id = "microsoft/Florence-2-base-ft" # NOTE: must use fine-tuned model (base or large)
-model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-base-ft", trust_remote_code=True, revision='refs/pr/6').to(device)
+model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-base-ft", trust_remote_code=True, revision='refs/pr/6')
+model.to(device)
 processor = AutoProcessor.from_pretrained("microsoft/Florence-2-base-ft", trust_remote_code=True, revision='refs/pr/6')
 
 # model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-large-ft", trust_remote_code=True, revision='refs/pr/10').to(device)
@@ -62,19 +63,25 @@ def main():
     for batch in tqdm(loader, desc="Processing batches", unit="batch"):
         image_paths, captions, uids = batch
         captions = list(captions)
-        try: 
-            scores = forward(image_paths, captions) # forward pass here
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            print("Image paths that caused the error:")
-            for path in image_paths:
-                print(path)
-                try:
-                    file_size = os.path.getsize(path)
-                    print(f"File size: {file_size} bytes")
-                except OSError as e:
-                    print(f"Could not get file size for {path}: {e}")
-            scores = torch.ones(len(image_paths)) * -1 # filter out these invalid samples later
+        # try: 
+        
+        import time
+        start_time = time.time()
+        scores = forward_debug(image_paths, captions) # forward pass here
+        end_time = time.time()
+        print(f"Forward pass took {end_time - start_time} seconds")
+        exit()
+        # except Exception as e:
+        #     print(f"Error occurred: {e}")
+        #     print("Image paths that caused the error:")
+        #     for path in image_paths:
+        #         print(path)
+        #         try:
+        #             file_size = os.path.getsize(path)
+        #             print(f"File size: {file_size} bytes")
+        #         except OSError as e:
+        #             print(f"Could not get file size for {path}: {e}")
+        #     scores = torch.ones(len(image_paths)) * -1 # filter out these invalid samples later
         if not isinstance(scores, list):
             scores = scores.tolist()
         batch_results = list(zip(uids, scores))
@@ -278,19 +285,114 @@ loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
 
 #     return loss_list
 
-def forward(image_paths, texts):
-    """
-    Perplexity-based implementation of VQAscore using the florence-2 model
+import time
 
-    TODO: 
-        - modify so I don't repeat tokenization twice
-        - if the processor truncates, then just calculate the loss for part of the caption that is not truncated - test with dense_caption dataset
+def forward_debug(image_paths, texts):
     """
-    # Prepare inputs
-    texts = [" " + text for text in texts]
-    original_texts = texts.copy()
+    Florence-2 Perplexity Score Implementation by Haoli Yin
+    """
+
+    start_time = time.time()
+
+    # Prepare inputs, add space to the beginning of each text for tokenization consistency
+    texts = [" " + text for text in texts] 
     texts = [default_question_template.format(text) for text in texts]
     images = [Image.open(image_path).convert("RGB") for image_path in image_paths]
+
+    prepare_inputs_time = time.time()
+    print(f"Time to prepare inputs: {prepare_inputs_time - start_time} seconds")
+
+    # must do max length padding since attention masking in the model doesn't currently work
+    inputs = processor(text=texts, images=images, return_tensors="pt", padding="max_length", truncation=True, max_length=1024)
+
+    move_to_device_time = time.time()
+    print(f"Time to process inputs: {move_to_device_time - prepare_inputs_time} seconds")
+
+    # move to device
+    input_ids = inputs.input_ids.to(device)
+    pixel_values = inputs.pixel_values.to(device)
+    attention_mask = inputs.attention_mask.to(device)
+
+    find_indices_time = time.time()
+    print(f"Time to move inputs to device: {find_indices_time - move_to_device_time} seconds")
+
+    # find the caption-specific token indices for perplexity calculation
+    start_idx = 11 # this value changes if the question template changes
+    end_idx = (input_ids == 2).long().argmax(dim=1) # argmax_cuda doesn't support bool
+
+    create_decoder_input_ids_time = time.time()
+    print(f"Time to find caption-specific token indices: {create_decoder_input_ids_time - find_indices_time} seconds")
+
+    # Create decoder_input_ids by shifting the input_ids right
+    decoder_input_ids = input_ids.clone()
+    decoder_input_ids[:, :-1] = input_ids[:, 1:]
+    decoder_input_ids[:, -1] = processor.tokenizer.eos_token_id
+
+    forward_pass_start_time = time.time()
+    print(f"Time to create decoder input ids: {forward_pass_start_time - create_decoder_input_ids_time} seconds")
+
+    # Forward pass
+    with torch.no_grad():
+        logits = model(
+            input_ids=input_ids,
+            pixel_values=pixel_values,
+            attention_mask=attention_mask,
+            decoder_input_ids=decoder_input_ids,
+        ).logits
+
+    forward_pass_end_time = time.time()
+    print(f"Time for forward pass: {forward_pass_end_time - forward_pass_start_time} seconds")
+
+    batch_size, seq_len, vocab_size = logits.shape
+    max_length = (end_idx - start_idx).max().item()
+
+    init_tensors_time = time.time()
+    print(f"Time to get logits shape and max length: {init_tensors_time - forward_pass_end_time} seconds")
+
+    # init tensors with -100 padding to max length for efficient batched CE loss calculation
+    padded_logits = torch.full((batch_size, max_length, vocab_size), -100.0, device=device)
+    padded_input_ids = torch.full((batch_size, max_length), -100, device=device)
+
+    fill_tensors_start_time = time.time()
+    print(f"Time to initialize tensors: {fill_tensors_start_time - init_tensors_time} seconds")
+
+    # Fill in the relevant portions
+    for i in range(batch_size):
+        length = end_idx[i] - start_idx
+        padded_logits[i, :length] = logits[i, start_idx-2:end_idx[i]-2] # account for decoder offset
+        padded_input_ids[i, :length] = input_ids[i, start_idx:end_idx[i]]
+
+    fill_tensors_end_time = time.time()
+    print(f"Time to fill tensors: {fill_tensors_end_time - fill_tensors_start_time} seconds")
+
+    # calculate per-token loss and perplexity
+    loss = F.cross_entropy(padded_logits.view(-1, vocab_size), 
+                           padded_input_ids.view(-1), 
+                           ignore_index=-100, 
+                           reduction='none')
+    loss = loss.view(batch_size, -1)
+    perplexity = torch.exp(loss.sum(dim=1) / (end_idx - start_idx).float())
+
+    calculate_loss_time = time.time()
+    print(f"Time to calculate loss and perplexity: {calculate_loss_time - fill_tensors_end_time} seconds")
+
+    total_time = time.time()
+    print(f"Total time for forward_debug: {total_time - start_time} seconds")
+
+    return perplexity.tolist()
+
+
+def forward(image_paths, texts):
+    """
+    Florence-2 Perplexity Score Implementation by Haoli Yin
+    """
+
+    # Prepare inputs, add space to the beginning of each text for tokenization consistency
+    texts = [" " + text for text in texts] 
+    texts = [default_question_template.format(text) for text in texts]
+    images = [Image.open(image_path).convert("RGB") for image_path in image_paths]
+
+    # must do max length padding since attention masking in the model doesn't currently work
     inputs = processor(text=texts, images=images, return_tensors="pt", padding="max_length", truncation=True, max_length=1024)
 
     # move to device
@@ -298,9 +400,9 @@ def forward(image_paths, texts):
     pixel_values = inputs.pixel_values.to(device)
     attention_mask = inputs.attention_mask.to(device)
 
-    # find the caption token indices for later
+    # find the caption-specific token indices for perplexity calculation
     start_idx = 11 # this value changes if the question template changes
-    end_idx = (input_ids == 2).to(torch.int8).argmax(dim=1)
+    end_idx = (input_ids == 2).long().argmax(dim=1) # argmax_cuda doesn't support bool
 
     # Create decoder_input_ids by shifting the input_ids right
     decoder_input_ids = input_ids.clone()
@@ -309,65 +411,58 @@ def forward(image_paths, texts):
 
     # Forward pass
     with torch.no_grad():
-        outputs = model(
+        logits = model(
             input_ids=input_ids,
             pixel_values=pixel_values,
             attention_mask=attention_mask,
             decoder_input_ids=decoder_input_ids,
-        )
+        ).logits
 
-    logits = outputs.logits
+    batch_size, seq_len, vocab_size = logits.shape
+    max_length = (end_idx - start_idx).max().item()
 
-    print("output logits ids")
-    print(torch.argmax(logits, dim=-1))
-    # well it's not always going to be the argmax, that's why the perplexity isn't 0 duh
+    # init tensors with -100 padding to max length for efficient batched CE loss calculation
+    padded_logits = torch.full((batch_size, max_length, vocab_size), -100.0, device=device)
+    padded_input_ids = torch.full((batch_size, max_length), -100, device=device)
 
-    scores = []
-    for i, (text, original_text) in enumerate(zip(texts, original_texts)):
-        # Get the logits and input_ids for this specific input
-        input_logits = logits[i]
+    # Fill in the relevant portions
+    for i in range(batch_size):
+        length = end_idx[i] - start_idx
+        padded_logits[i, :length] = logits[i, start_idx-2:end_idx[i]-2] # account for decoder offset
+        padded_input_ids[i, :length] = input_ids[i, start_idx:end_idx[i]]
 
-        # Calculate loss for each token in the original text
-        # try: 
-        loss = F.cross_entropy(input_logits[:, start_idx:end_idx[i]], input_ids[:, start_idx:end_idx[i]], reduction='none')
-        # Calculate perplexity
-        perplexity = torch.exp(loss.mean())
-        scores.append(perplexity.item())
-        # except: 
-        #     print(f"Error calculating loss for sample {i}")
-        #     print(f"Input logits: {input_logits}")
-        #     print(f"Start index: {start_idx}")
-        #     print(f"End index: {end_idx}")
-        #     print(f"file name: {image_paths[i]}")
-        #     print(f"text: {text}")
-        #     print(f"original text: {original_text}")
-        #     scores.append(-1)
-
-    return scores
+    # calculate per-token loss and perplexity
+    loss = F.cross_entropy(padded_logits.view(-1, vocab_size), 
+                           padded_input_ids.view(-1), 
+                           ignore_index=-100, 
+                           reduction='none')
+    loss = loss.view(batch_size, -1)
+    perplexity = torch.exp(loss.sum(dim=1) / (end_idx - start_idx).float())
+    return perplexity.tolist()
 
 if __name__ == "__main__":
 
-    # main()
+    main()
 
     
-    # FIXME: known issue that batching influences scores within the batch for some reason. One factor is that padding to longest in batch influences decoding scores
-    # this is because the attention mask that usually ignores padding tokens is not used in generate, which causes the model to attend over the padding tokens
-    # For example, if you set padding="max_length", the resulting scores for both true and false ground truth are relatively low scores.
+    # # FIXME: known issue that batching influences scores within the batch for some reason. One factor is that padding to longest in batch influences decoding scores
+    # # this is because the attention mask that usually ignores padding tokens is not used in generate, which causes the model to attend over the padding tokens
+    # # For example, if you set padding="max_length", the resulting scores for both true and false ground truth are relatively low scores.
      
-    ## unit test: batched inference
-    image_path_1 = "data/sampled_datacomp/sampled_datacomp_images/000000000002.jpg"
-    caption_1 = "The image features a Mercedes-Benz G-Class SUV, portrayed in a professional studio setting. The vehicle sports a metallic gray finish, with a robust and boxy design that emphasizes its luxury and off-road capabilities. It features distinctive elements such as a bold front grille adorned with the Mercedes-Benz logo, round LED headlights, and a prominent front bumper. The SUV's rugged appeal is enhanced by its pronounced wheel arches housing large, stylish alloy wheels. The side profile reveals sturdy running boards and a high ground clearance, typical of a vehicle designed for both urban and off-road use. The overall aesthetic combines a traditional SUV silhouette with modern, high-end details, presenting a vehicle that exudes both luxury and utility." # positive test case
-    image_path_2 = "data/sampled_datacomp/sampled_datacomp_images/000000000007.jpg"
-    caption_2 = "bad example what is this" # negative test case
-    image_path_3 = "data/sampled_datacomp/sampled_datacomp_images/000000004230.jpg"
-    caption_3 = "the highlighted vested woman is on the left of the man with a blue shirt" # negative test case
+    # ## unit test: batched inference
+    # image_path_1 = "data/sampled_datacomp/sampled_datacomp_images/000000000002.jpg"
+    # caption_1 = "The image features a Mercedes-Benz G-Class SUV, portrayed in a professional studio setting. The vehicle sports a metallic gray finish, with a robust and boxy design that emphasizes its luxury and off-road capabilities. It features distinctive elements such as a bold front grille adorned with the Mercedes-Benz logo, round LED headlights, and a prominent front bumper. The SUV's rugged appeal is enhanced by its pronounced wheel arches housing large, stylish alloy wheels. The side profile reveals sturdy running boards and a high ground clearance, typical of a vehicle designed for both urban and off-road use. The overall aesthetic combines a traditional SUV silhouette with modern, high-end details, presenting a vehicle that exudes both luxury and utility." # positive test case
+    # image_path_2 = "data/sampled_datacomp/sampled_datacomp_images/000000000007.jpg"
+    # caption_2 = "bad example what is this" # negative test case
+    # image_path_3 = "data/sampled_datacomp/sampled_datacomp_images/000000004230.jpg"
+    # caption_3 = "the highlighted vested woman is on the left of the man with a blue shirt" # negative test case
 
-    repeated_caption_1 = caption_3
+    # repeated_caption_1 = caption_3
 
     # print("Testing batched inference")
-    images, captions = [image_path_1, image_path_2, image_path_3], [caption_1, caption_2, caption_3]
-    scores = forward(images, captions)
-    print(scores)
+    # images, captions = [image_path_1, image_path_2, image_path_3], [caption_1, caption_2, caption_3]
+    # scores = forward(images, captions)
+    # print(scores)
 
     # print("Testing 3x single forward inference")
     # for image, caption in zip(images, captions):
